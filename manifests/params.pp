822b470e099e394fd1c922ea3e8355d0dece8081
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2015-11-09 15:21:12 +0000

    Fix variable name

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,7 +13,7 @@ class freetds::params {
       $tds_packages = ['freetds', 'freetds-devel']
       $odbc_packages = ['unixODBC', 'unixODBC-devel']
       $odbc_lib = '/usr/lib'
-      $odbc_lib = '/usr/lib64'
+      $odbc_lib64 = '/usr/lib64'
       $freetds_conf = '/etc/freetds.conf'
     }
     default: {}
