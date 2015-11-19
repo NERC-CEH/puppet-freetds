737e76340bf171070d8169339aea3f35d10107cf
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2015-11-19 14:28:20 +0000

    Update spec tests, generated with retrospec

--- spec/classes/freetds_spec.rb
+++ spec/classes/freetds_spec.rb
@@ -1,87 +1,117 @@
 require 'spec_helper'
+require 'shared_contexts'
 
-describe 'freetds', :type => :class do   
-  describe 'when odbc is managed' do
-    let(:params) { {
-      :manage_unixodbc => true
-    } }
-      
-    it { should contain_package('unixodbc') }
-    it { should contain_package('unixodbc-dev') }
+describe 'freetds' do
+  # by default the hiera integration uses hiera data from the shared_contexts.rb file
+  # but basically to mock hiera you first need to add a key/value pair
+  # to the specific context in the spec/shared_contexts.rb file
+  # Note: you can only use a single hiera context per describe/context block
+  # rspec-puppet does not allow you to swap out hiera data on a per test block
+  #include_context :hiera
+
+  
+  # below is the facts hash that gives you the ability to mock
+  # facts on a per describe/context block.  If you use a fact in your
+  # manifest you should mock the facts below.
+  let(:facts) do
+    {}
   end
-
-  describe 'when odbc is not managed' do
-    let(:params) { {
-      :manage_unixodbc => false
-    } }
-      
-    it { should_not contain_package('unixodbc') }
-    it { should_not contain_package('unixodbc-dev') }
+  # below is a list of the resource parameters that you can override.
+  # By default all non-required parameters are commented out,
+  # while all required parameters will require you to add a value
+  let(:params) do
+    {
+      #:manage_unixodbc => true,
+      #:unixodbc_version => installed,
+      #:freetds_version => installed,
+      #:global_tds_version => "7.0",
+      #:global_port => 1433,
+    }
   end
-
-  describe 'when freetds version is specified' do
-    let(:params) { {
-      :freetds_version => '2'
-    } }
-    
-    it { should contain_package('freetds-dev').with_ensure('2') }
-    it { should contain_package('freetds-bin').with_ensure('2') }
-    it { should contain_package('tdsodbc').with_ensure('2') }
+  # add these two lines in a single test block to enable puppet and hiera debug mode
+  # Puppet::Util::Log.level = :debug
+  # Puppet::Util::Log.newdestination(:console)
+  it do
+    is_expected.to contain_package('$::osfamily ? { Debian => [freetds-dev, freetds-bin, tdsodbc], RedHat => [freetds, freetds-devel], default => undef }')
+      .with(
+        'ensure' => 'installed'
+      )
   end
-
-  describe 'when applied should manage /etc/odbcinst.ini' do
-    it { should contain_ini_setting('FreeTDS Description').with(
-      :path    => '/etc/odbcinst.ini',
-      :section => 'FreeTDS',
-      :setting => 'Description',
-      :value   => 'FreeTDS Driver'
-    ).with_ensure('present')}
-
-    it { should contain_ini_setting('FreeTDS Driver').with(
-      :path    => '/etc/odbcinst.ini',
-      :section => 'FreeTDS',
-      :setting => 'Driver',
-      :value   => '/usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so'
-    ).with_ensure('present')}
-
-    it { should contain_ini_setting('FreeTDS Setup').with(
-      :path    => '/etc/odbcinst.ini',
-      :section => 'FreeTDS',
-      :setting => 'Setup',
-      :value   => '/usr/lib/x86_64-linux-gnu/odbc/libtdsS.so'
-    ).with_ensure('present')}
+  it do
+    is_expected.to contain_ini_setting('FreeTDS Description')
+      .with(
+        'path'    => '/etc/odbcinst.ini',
+        'section' => 'FreeTDS',
+        'setting' => 'Description',
+        'value'   => 'ODBC for TDB'
+      )
   end
-
-  describe 'when applied should manage the global port' do
-    let(:params) { {
-      :global_port => '1337'
-    } }
-
-    it { should contain_ini_setting('FreeTDS Global Port').with(
-      :path    => '/etc/freetds/freetds.conf',
-      :section => 'global',
-      :setting => 'port',
-      :value   => '1337'
-     ).with_ensure('present')}
+  it do
+    is_expected.to contain_ini_setting('FreeTDS Driver')
+      .with(
+        'path'    => '/etc/odbcinst.ini',
+        'section' => 'FreeTDS',
+        'setting' => 'Driver',
+        'value'   => '$::osfamily ? { Debian => /usr/lib/i386-linux-gnu/odbc, RedHat => /usr/lib, default => undef }/libtdsodbc.so'
+      )
   end
-
-  describe 'when applied should mange the global tds version' do
-    let(:params) { {
-      :global_tds_version => '18'
-    } }    
-   
-    it { should contain_ini_setting('FreeTDS Global tds version').with(
-      :path    => '/etc/freetds/freetds.conf',
-      :section => 'global',
-      :setting => 'tds version',
-      :value   => '18'
-    ).with_ensure('present')}
+  it do
+    is_expected.to contain_ini_setting('FreeTDS Setup')
+      .with(
+        'path'    => '/etc/odbcinst.ini',
+        'section' => 'FreeTDS',
+        'setting' => 'Setup',
+        'value'   => '$::osfamily ? { Debian => /usr/lib/i386-linux-gnu/odbc, RedHat => /usr/lib, default => undef }/libtdsS.so'
+      )
   end
-
-  describe 'the freetds conf should be managed before freetds is install' do
-    it { should contain_ini_setting('FreeTDS Global tds version')
-                .that_comes_before('Package[freetds-bin]')}
-    it { should contain_ini_setting('FreeTDS Global Port')
-                .that_comes_before('Package[freetds-bin]')}
+  it do
+    is_expected.to contain_ini_setting('FreeTDS Driver64')
+      .with(
+        'path'    => '/etc/odbcinst.ini',
+        'section' => 'FreeTDS',
+        'setting' => 'Driver64',
+        'value'   => '$::osfamily ? { Debian => /usr/lib/x86_64-linux-gnu/odbc, RedHat => /usr/lib64, default => undef }/libtdsodbc.so'
+      )
+  end
+  it do
+    is_expected.to contain_ini_setting('FreeTDS Setup64')
+      .with(
+        'path'    => '/etc/odbcinst.ini',
+        'section' => 'FreeTDS',
+        'setting' => 'Setup64',
+        'value'   => '$::osfamily ? { Debian => /usr/lib/x86_64-linux-gnu/odbc, RedHat => /usr/lib64, default => undef }/libtdsS.so'
+      )
+  end
+  it do
+    is_expected.to contain_file('/etc/freetds')
+      .with(
+        'ensure' => 'directory'
+      )
+  end
+  it do
+    is_expected.to contain_ini_setting('FreeTDS Global Port')
+      .with(
+        'before'  => 'Package[$tds_packages]',
+        'path'    => '$::osfamily ? { Debian => /etc/freetds/freetds.conf, RedHat => /etc/freetds.conf, default => undef }',
+        'section' => 'global',
+        'setting' => 'port',
+        'value'   => '1433'
+      )
+  end
+  it do
+    is_expected.to contain_ini_setting('FreeTDS Global tds version')
+      .with(
+        'before'  => 'Package[$tds_packages]',
+        'path'    => '$::osfamily ? { Debian => /etc/freetds/freetds.conf, RedHat => /etc/freetds.conf, default => undef }',
+        'section' => 'global',
+        'setting' => 'tds version',
+        'value'   => '7.0'
+      )
+  end
+  it do
+    is_expected.to contain_package('$::osfamily ? { Debian => [unixodbc, unixodbc-dev], RedHat => [unixODBC, unixODBC-devel], default => undef }')
+      .with(
+        'ensure' => 'installed'
+      )
   end
 end
