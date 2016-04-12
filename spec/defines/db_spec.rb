43496de475d52d4788e4c0231d2d050d779cb1cd
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2016-04-12 11:35:38 +0100

    Revert "Update spec tests, generated with retrospec"
    This reverts commit 737e76340bf171070d8169339aea3f35d10107cf.

--- spec/defines/db_spec.rb
+++ spec/defines/db_spec.rb
@@ -1,85 +1,48 @@
 require 'spec_helper'
-require 'shared_contexts'
 
-describe 'freetds::db' do
-  # by default the hiera integration uses hiera data from the shared_contexts.rb file
-  # but basically to mock hiera you first need to add a key/value pair
-  # to the specific context in the spec/shared_contexts.rb file
-  # Note: you can only use a single hiera context per describe/context block
-  # rspec-puppet does not allow you to swap out hiera data on a per test block
-  #include_context :hiera
+describe 'freetds::db', :type => :define do
+  let(:pre_condition) { 'include freetds' }
+  let(:title) { 'test-db' }
 
-  let(:title) { 'XXreplace_meXX' }
-  
-  # below is the facts hash that gives you the ability to mock
-  # facts on a per describe/context block.  If you use a fact in your
-  # manifest you should mock the facts below.
-  let(:facts) do
-    {}
+  describe 'when applied, dsn should be created' do
+    let(:params) { { :host => 'myhost' } }
+
+    it { should contain_ini_setting('FreeTDS connection to test-db database').with(
+      :path    => '/etc/freetds/freetds.conf',
+      :section => 'test-db',
+      :setting => 'host',
+      :value   => 'myhost'
+    ).with_ensure('present')}
   end
-  # below is a list of the resource parameters that you can override.
-  # By default all non-required parameters are commented out,
-  # while all required parameters will require you to add a value
-  let(:params) do
-    {
-      :host => 'place_value_here',
-      #:dsn => $title,
-      #:port => undef,
-      #:tds_version => undef,
-      #:manage_odbc => true,
-    }
+
+  describe 'when odbc is managed' do
+    let(:params) { { 
+      :manage_odbc => true,
+      :host        => 'myhost'
+    } }
+
+    it { should contain_ini_setting('ODBC Driver to test-db').with(
+      :path    => '/etc/odbc.ini',
+      :section => 'test-db',
+      :setting => 'Driver',
+      :value   => 'FreeTDS'
+    ).with_ensure('present')}
+
+    it { should contain_ini_setting('ODBC Servername to test-db').with(
+      :path    => '/etc/odbc.ini',
+      :section => 'test-db',
+      :setting => 'Servername',
+      :value   => 'test-db'
+    ).with_ensure('present')}
   end
-  # add these two lines in a single test block to enable puppet and hiera debug mode
-  # Puppet::Util::Log.level = :debug
-  # Puppet::Util::Log.newdestination(:console)
-  it do
-    is_expected.to contain_ini_setting('FreeTDS connection to $title database')
-      .with(
-        'ensure'  => 'present',
-        'path'    => '$::osfamily ? { Debian => /etc/freetds/freetds.conf, RedHat => /etc/freetds.conf, default => undef }',
-        'section' => '$title',
-        'setting' => 'host',
-        'value'   => ''
-      )
-  end
-  it do
-    is_expected.to contain_ini_setting('ODBC Driver to $title')
-      .with(
-        'ensure'  => 'present',
-        'path'    => '/etc/odbc.ini',
-        'section' => '$title',
-        'setting' => 'Driver',
-        'value'   => 'FreeTDS'
-      )
-  end
-  it do
-    is_expected.to contain_ini_setting('ODBC Servername to $title')
-      .with(
-        'ensure'  => 'present',
-        'path'    => '/etc/odbc.ini',
-        'section' => '$title',
-        'setting' => 'Servername',
-        'value'   => '$title'
-      )
-  end
-  it do
-    is_expected.to contain_ini_setting('FreeTDS port to $title database')
-      .with(
-        'ensure'  => 'present',
-        'path'    => '$::osfamily ? { Debian => /etc/freetds/freetds.conf, RedHat => /etc/freetds.conf, default => undef }',
-        'section' => '$title',
-        'setting' => 'port',
-        'value'   => 'undef'
-      )
-  end
-  it do
-    is_expected.to contain_ini_setting('FreeTDS version to $title database')
-      .with(
-        'ensure'  => 'present',
-        'path'    => '$::osfamily ? { Debian => /etc/freetds/freetds.conf, RedHat => /etc/freetds.conf, default => undef }',
-        'section' => '$title',
-        'setting' => 'tds version',
-        'value'   => 'undef'
-      )
+
+  describe 'when odbc is not managed' do
+    let(:params) { {
+      :manage_odbc => false,
+      :host        => 'host'
+    } }
+
+    it { should_not contain_ini_setting('ODBC Driver to test-db') }
+    it { should_not contain_ini_setting('ODBC Servername to test-db') }
   end
 end
